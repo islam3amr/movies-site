3962428bc312ab8c901c28cc1c38cee7f8ad44ad
islam3amr <eslamamrshaban29@gmail.com>
2026-02-08 02:43:32 +0200

    Fix GitHub Pages + TMDB secret

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/movies-site/",   // مهم جدًا لـ GitHub Pages
-  plugins: [react()],
+  base: "/movies-site/",
 });
+
